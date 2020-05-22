afbb301a8a956b611e9bd23d37ecb9c3f1affb71
Ryan <mersmannryan@gmail.com>
2020-05-21 18:37:48 -0600

    added styled components to header and footer

--- african-marketplace-front-end/src/components/footer.js
+++ african-marketplace-front-end/src/components/footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Footer } from 'styled-components';
 
 function footer() {
     return (
-        <div className="footer">
+        <Footer>
             <h1>African Marketplace</h1>
             <div class="copyright">
                 © 2020 Copyright: Build-Week-African-Marketplace-III
             </div>
-        </div>
+        </Footer>
     )
 }
 
--- african-marketplace-front-end/src/components/header.js
+++ african-marketplace-front-end/src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import { Header } from 'styled-components';
 
 function header() {
     return (
-        <div className="footer">
+        <Header>
             <h1>African Marketplace</h1>
             <nav>
                 <Link to={`/`}>Home</Link>
@@ -11,7 +12,7 @@ function header() {
                 <Link to={`/order`}>Order Now</Link>
                 <Link to={`/contact`}>Contact Us</Link>
             </nav>
-        </div>
+        </Header>
     )
 }
 
